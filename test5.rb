191211106fcba6aea8591ec944d0746bb9a79267
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-09-12 00:45:07 +0200

    test_block

--- test5.rb
+++ test5.rb
@@ -1,19 +1,42 @@
 
- def print_once
-    yield [1, 2, 3, 4]
-    pp "return num is #{num}"
- end
+def print_once()
+    pp "before reaching yield"
+     returned_value = yield 7, 1000
+    #num = yield([1, 2, 3, 4])
+    #pp "return num is #{num}"
+    pp "After  yield"
+    pp "making use of the double and its value is #{returned_value} and its value minus 1 is #{ returned_value - 1}"
+end
 
 
 
 
 
 
-  print_once  do |value|
-    num = 5
-    pp "Block is being run, my value is #{value}" 
-   
-    next num
-  end
+  #rint_once 
+  
+ print_once do |vsrklfglhklhjlklfgknlhjk, kkkkk|  
+   pp "IN the yield   kkkkkk value is #{kkkkk}"
+   pp "im making use of the number that was passed to me which is #{vsrklfglhklhjlklfgknlhjk}"
+   pp "it's double is #{vsrklfglhklhjlklfgknlhjk * 2}"
+   vsrklfglhklhjlklfgknlhjk * 3
+end
+
+
+
+
+
+
+
+
+#pp my_var
+
+
+#   do 
+#     pp "
+#     value is " 
+#     #num = 5
+#     #return num
+#   end
 
 
